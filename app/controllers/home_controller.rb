ae15d4ea369ec2b899e469d6277e6dc56371de5b
tianscholtz <tian.scholtz@gmail.com>
2013-03-25 13:26:01 +0200

    Entries list partial created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+
+    # Read in all entries in order to display on home page
     @entries = Entry.all
 
     respond_to do |format|
