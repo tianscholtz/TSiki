783560019122f9b405dd802974c0c3df9656a7ca
tianscholtz <tian.scholtz@gmail.com>
2013-03-25 09:42:17 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TSiki::Application.config.secret_token = '4e13ee76697452c1ff619a3d3fb41ef5788b0263fe2ac0bd9781d31872c7821280435759a540b357efdecc104295453d9aa6e9d4cb7b928ab3fb2398f6390bb0'
